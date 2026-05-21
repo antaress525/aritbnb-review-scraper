d1c6e45fdafed89b5ae2c8d9fd86edcd0ba2722a
antaress <nanatchendopjoelivan@gmail.com>
2026-05-21 02:54:21 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from selectolax.parser import HTMLParser
+from playwright.sync_api import sync_playwright
+
+def main():
+    print("Hello from airbnb-comment-scraper!")
+
+
+if __name__ == "__main__":
+    main()
